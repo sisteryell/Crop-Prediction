1aa0d1a65d64b4cc95aafab8f16bfa00277eeec2
Md Azhar Maqsood <mdazharmaqsood@gmail.com>
2025-07-14 16:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import numpy as np
 from flask import Flask,request,render_template
 import pickle
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 model = pickle.load(open("model.pkl","rb"))
 
-@flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/predict",methods=["POST"])
+@app.route("/predict",methods=["POST"])
 def predict():
     float_feature = [float(x) for x in request.form.values()]
     features = [np.array(float_feature)]
@@ -17,4 +17,4 @@ def predict():
     return render_template("index.html",prediction_text=f"The predicted crop is {prediction}")
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
